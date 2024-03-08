2754df1aaf3e14cb81f562d1a37d95e82a5ea179
lww <781115101@qq.com>
2024-03-08 16:11:24 +0800

    fix

--- www/laravel/app/Services/TestService.php
+++ www/laravel/app/Services/TestService.php
@@ -9,6 +9,17 @@ class TestService
      */
     public function test()
     {
-
+        // 业务逻辑
+        return [
+            'name'    => 'test',
+            'age'     => 20,
+            'sex'     => 'man',
+            'address' => 'china',
+            'email'   => 'test@qq.com',
+            'phone'   => '13812345678',
+            'qq'      => '123456789',
+            'wechat'  => '123456789',
+            'weibo'   => '123456789',
+        ];
     }
 }
